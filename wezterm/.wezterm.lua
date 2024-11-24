ce1fa6ddf10b56151a5a7e6b9849f8534a558e57
AlHind AHITTA <tipsgamer596@gmail.com>
2024-11-24 16:10:01 +0530

    new things can't write a commit message

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,17 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
-config.enable_kitty_graphics = true
-config.default_prog = { "cygwin" }
+config.default_prog = { "powershell" }
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.font = wezterm.font 'JetBrains Mono'
+config.window_padding = {
+  left = 5,
+  right = 5,
+  top = 0,
+  bottom = 8,
+}
+
+config.window_background_opacity = 0.8
 
 return config
